460fdcd8e6de82bc613547d5c4760787b92b6b81
Gulshan Singh <gulshan@umich.edu>
2012-12-20 00:18:25 -0600

    Added page title to template

--- index.php
+++ index.php
@@ -6,8 +6,9 @@
 		<?php
 			$page_id = get_the_ID();
 			$page_data = get_page($page_id);
-			echo $page_data->post_content;
 		?>
+                <div class="title"><?php echo $page_data->post_title; ?></div>
+                <?php echo $page_data->post_content; ?>
 	</div> <!-- #content -->
 	
 	<div id="primary" class="widget-area">
