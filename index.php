a99995b7e1aee72201981380e9efee9dd9fff48f
Gulshan Singh <gulshan@umich.edu>
2012-12-22 12:59:45 -0600

    Created a two column layout

--- index.php
+++ index.php
@@ -1,16 +1,21 @@
 <?php get_header(); ?>
 
 <div id="container">
-	<?php get_sidebar(); ?>
 	<div id="content">
-		<?php
-			$page_id = get_the_ID();
+                <?php get_sidebar(); ?>
+                <div class="post">
+		    <?php
+		        $page_id = get_the_ID();
 			$page_data = get_page($page_id);
-		?>
-                <div class="post_title"><?php echo $page_data->post_title; ?></div>
-		<div class="post_content">
-		 <?php echo $page_data->post_content; ?>
-		</div>
+		    ?>
+                    <div class="post_title"><?php echo $page_data->post_title; ?></div>
+		    <div class="post_content">
+		        <?php echo $page_data->post_content; ?>
+		    </div>
+		</div> <!-- .post -->
+		
+		<!-- This div is a hack that allows us to acheive a proper two column layout -->
+		<div style="clear: both"></div>
 	</div> <!-- #content -->
 	
 </div> <!-- #container -->
