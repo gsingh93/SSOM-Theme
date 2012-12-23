57b3e59d88ddfe9963b73a57476dd3d2039e6908
Gulshan Singh <gulshan@umich.edu>
2012-12-22 23:00:22 -0600

    Made the sidebar go away with no widgets

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <div id="sidebar">
 <?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar()): ?>
-	<p style="padding: 10px 10px; padding-bottom: 0px;">No Sidebar Widgets</p>  
+	<!--<p style="padding: 10px 10px; padding-bottom: 0px;">No Sidebar Widgets</p>-->
 <?php endif; ?>  
 </div>
