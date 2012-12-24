c1a4487f256fdcc8c9c2ec9ec7b4e6ce2a72f7fb
Gulshan Singh <gulshan@umich.edu>
2012-12-24 05:58:07 -0600

    Added Google Analytics tracking

--- header.php
+++ header.php
@@ -2,6 +2,18 @@
 <html>
 	<head>
 		<title>Sikh Society of Michigan</title>
+ 
+		<script type="text/javascript">
+		  var _gaq = _gaq || [];
+		  _gaq.push(['_setAccount', 'UA-37238415-1']);
+		  _gaq.push(['_trackPageview']);
+
+		  (function() {
+		  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		  })();
+		</script>
 	
 		<meta http-equiv="content-type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 			
