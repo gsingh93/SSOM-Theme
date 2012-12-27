cab6d9df9626c13c697d858f777e5bd99d29c154
Gulshan Singh <gulshan@umich.edu>
2012-12-27 14:01:22 -0600

    Fixed the 404 page

--- 404.php
+++ 404.php
@@ -1,2 +1,19 @@
-<h1>404 Error</h1>
-<p>The page you are trying to access does not exist.</p>
+<?php get_header(); ?>
+
+<div id="container">
+	<div id="content">
+                <?php get_sidebar(); ?>
+                <div class="post">
+                    <div class="post_title">Page Not Found</div>
+		    <div class="post_content">
+                        <p>Sorry, we couldn't find the page you requested.</p>
+		    </div>
+		</div> <!-- .post -->
+		
+		<!-- This div is a hack that allows us to acheive a proper two column layout -->
+		<div style="clear: both"></div>
+	</div> <!-- #content -->
+	
+</div> <!-- #container -->
+
+<?php get_footer(); ?>
