333d8da46892b6d33b35622d712b71927b4b3ec5
Gulshan Singh <gulshan@umich.edu>
2012-12-31 14:43:48 -0600

    Fixed footer text

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@ A Non-Profit Organization<br>
 33340 Dequindre, Sterling Heights, MI - 48310 (<a href="http://goo.gl/maps/G4B4M" style="color:#0FC;">View Map</a>)<br>
 Tel : 248-547-0927<br><br>
 
-For Urgent Matters Contact Surinderjit Singh: 586-873-3444<br>
+For urgent matters contact Surinderjit Singh: 586-873-3444<br>
 
 Please send website comments to the webmaster (Gulshan Singh) at: <a href="mailto:webmaster@michigangurudwara.com" style="color:#0FC;">webmaster@michigangurudwara.com</a><br>
 This web site is best viewed using Firefox 3.5 and above.
