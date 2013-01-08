db2c4eab7d54c9e6906fcde0a44c8741f0eeeee1
Gulshan Singh <gulshan@umich.edu>
2013-01-07 23:28:26 -0600

    Registered a custom homework post type

--- functions.php
+++ functions.php
@@ -21,3 +21,34 @@ function get_title() {
 function get_description() {
 	echo "Madison Heights Gurdwara";
 }
+
+/*function my_remove_meta_box() { remove_meta_box('pageparentdiv', 'homework', 'normal'); };
+function my_add_meta_box() { add_meta_box('chapter-parent', 'Part', 'chapter_attributes_meta_box', 'homework', 'side', 'high');};
+add_action('admin_menu', 'my_remove_meta_box');
+add_action('add_meta_boxes', 'my_add_meta_box');
+  function chapter_attributes_meta_box($post) {
+    $post_type_object = get_post_type_object($post->post_type);
+    if ( $post_type_object->hierarchical ) {
+      $pages = wp_dropdown_pages(array('post_type' => 'page', 'selected' => $post->post_parent, 'name' => 'parent_id', 'show_option_none' => __('(no parent)'), 'sort_column'=> 'menu_order, post_title', 'echo' => 0));
+      if ( ! empty($pages) ) {
+        echo $pages;
+      } // end empty pages check
+    } // end hierarchical check.
+  }*/
+
+register_post_type('homework', array(	'label' => 'Homework','description' => 'Punjabi class homework','public' => true,'show_ui' => true,'show_in_menu' => true,'capability_type' => 'post','hierarchical' => true,'rewrite' => array('slug' => ''),'query_var' => true,'exclude_from_search' => false,'supports' => array('title','editor','excerpt','trackbacks','custom-fields','comments','revisions','thumbnail','author','page-attributes',),'labels' => array (
+  'name' => 'Homework',
+  'singular_name' => 'Homework',
+  'menu_name' => 'Homework',
+  'add_new' => 'Add Homework',
+  'add_new_item' => 'Add New Homework',
+  'edit' => 'Edit',
+  'edit_item' => 'Edit Homework',
+  'new_item' => 'New Homework',
+  'view' => 'View Homework',
+  'view_item' => 'View Homework',
+  'search_items' => 'Search Homework',
+  'not_found' => 'No Homework Found',
+  'not_found_in_trash' => 'No Homework Found in Trash',
+  'parent' => 'Parent Homework',
+),) );
