c9d5971c706865a07897952e54f3d1e74c69a07b
Gulshan Singh <gulshan@umich.edu>
2013-01-08 23:48:56 -0500

    Added a filter to make sub menus in the navbar work

--- functions.php
+++ functions.php
@@ -21,3 +21,11 @@ function get_title() {
 function get_description() {
 	echo "Madison Heights Gurdwara";
 }
+
+add_filter( 'nav_menu_css_class', 'check_for_submenu', 10, 2);
+function check_for_submenu($classes, $item) {
+    global $wpdb;
+    $has_children = $wpdb->get_var("SELECT COUNT(meta_id) FROM wp_postmeta WHERE meta_key='_menu_item_menu_item_parent' AND meta_value='".$item->ID."'");
+    if ($has_children > 0) array_push($classes,'has-sub'); // add the class dropdown to the current list
+    return $classes;
+}
