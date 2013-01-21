9629a822e6009137b50d5780e9c90760fa4fda98
Gulshan Singh <gulshan@umich.edu>
2013-01-21 17:21:12 -0600

    Fixed post_id and added homework_below field to homework template

--- homework.php
+++ homework.php
@@ -13,33 +13,35 @@ Template Name: Homework List
                     <?php $post_data = get_post(get_the_ID()); ?>
 		    <div class="post_title"><?php echo $post_data->post_title; ?></div>
 		    <?php
-                        $grade = get_post_meta(get_the_ID(), 'grade', true);
-		        $posts = get_posts('post_type=homework&meta_key=grade&meta_value=' . $grade);
-?>
-<div class="post_content">
-<?php echo $post_data->post_content; ?>
-<table class="rounded-corners blue-table" id="langar">
-<thead>
-<tr>
-<td>Date</td>
-<td>Assignment</td>
-</tr>
-</thead>
-<?php
-foreach ($posts as $post) {
-  // Date format is yyyyddmm
-  $date = get_post_meta($post->ID, 'assign_date', true);
-  $date = substr($date, 4);
-  $date = substr($date, 0, 2) . '/' . substr($date, 2);
-
-  echo "<tr>";
-  echo "<td>" . $date . "</td>";
-  echo "<td><a href='" . get_permalink($post->ID) . "'>Visit Assignment</a></td>";
-  echo "</tr>";
-}
-                    ?>
-</table>
-</div>
+		        $id = $wp_query->post->ID;
+                        $grade = get_post_meta($id, 'grade', true);
+		        $posts = get_posts('post_type=homework&meta_key=grade&meta_value=' . $grade); 
+		    ?>
+		    <div class="post_content">
+		      <?php echo $post_data->post_content; ?>
+		      <table class="rounded-corners blue-table" id="langar">
+			<thead>
+			  <tr>
+			    <td>Date</td>
+			    <td>Assignment</td>
+			  </tr>
+			</thead>
+			<?php
+			   foreach ($posts as $post) {
+			   // Date format is yyyyddmm
+			   $date = get_post_meta($post->ID, 'assign_date', true);
+			$date = substr($date, 4);
+			$date = substr($date, 0, 2) . '/' . substr($date, 2);
+			
+			echo "<tr>";
+			  echo "<td>" . $date . "</td>";
+			  echo "<td><a href='" . get_permalink($post->ID) . "'>Visit Assignment</a></td>";
+			  echo "</tr>";
+			}
+			?>
+		      </table>
+		    </div>
+		    <?php echo get_post_meta($id, 'homework_below', true); ?>
 		</div> <!-- .post -->
 		
 		<!-- This div is a hack that allows us to acheive a proper two column layout -->
